36b52d7b150c979a4efb15a3960b8a1aaa2a0a1e
dongyuwei <newdongyuwei@gmail.com>
2021-04-06 16:19:52 +0800

    Initial Commit

---
+++ test-app/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  test-app
+//
+//  Created by dongyuwei on 2021/4/6.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ test-app/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  test-app
+//
+//  Created by dongyuwei on 2021/4/6.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
